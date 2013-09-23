10ba48a9219c13527be972ce46167c883cc53ac0
Robat Williams <robatwilliams@gmail.com>
2013-09-23 18:46:51 +0100

    Possible solution for allowing jQuery to be AMD-loaded.
    See https://github.com/knockout/knockout/issues/898

--- build/fragments/amd-pre.js
+++ build/fragments/amd-pre.js
@@ -3,12 +3,23 @@
     if (typeof require === 'function' && typeof exports === 'object' && typeof module === 'object') {
         // [1] CommonJS/Node.js
         var target = module['exports'] || exports; // module.exports is for Node.js
-        factory(target);
+        factory(target, jQuery);
     } else if (typeof define === 'function' && define['amd']) {
         // [2] AMD anonymous module
-        define(['exports'], factory);
+        define(['exports', 'module'], function (exports, module){
+            var config = module.config();
+            if (config.useFactory){
+                exports.factory = function (jQuery) {
+                    var ko = {};
+                    factory(ko, jQuery);
+                    return ko;
+                };
+            } else {
+               factory(exports, jQuery);
+            }
+        });
     } else {
         // [3] No module loader (plain <script> tag) - put directly in global namespace
-        factory(window['ko'] = {});
+        factory(window['ko'] = {}, jQuery);
     }
-}(function(koExports){
+}(function(koExports, jQuery){
